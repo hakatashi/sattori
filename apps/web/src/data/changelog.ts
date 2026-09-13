5473e82f23e90b69b2ed7dbace6e5039e044f1bb
Koki Takahashi <hakatasiloving@gmail.com>
2026-09-13 14:16:23 +0900

    feat(changelog): update display style for currently supported titles

--- apps/web/src/data/changelog.ts
+++ apps/web/src/data/changelog.ts
@@ -17,6 +17,11 @@ export interface ChangelogEntry {
 export const changelogEntries: ChangelogEntry[] = [
   {
     date: "2026-09-13",
+    ja: "「現在録画対応中のタイトル」の表示スタイルを修正",
+    en: "Fixed the display style of \"Currently supported titles\"",
+  },
+  {
+    date: "2026-09-13",
     ja: "東方紅魔郷: New Classic (th06nc) の録画に対応",
     en: "Added recording support for the Embodiment of Scarlet Devil: New Classic (th06nc)",
     issueUrl: "https://github.com/hakatashi/sattori-dev/issues/241",
